c3ba23d485c67e8118eb65804221aee0b988a35e
MateoF01 <mateofernandez380@gmail.com>
2024-03-25 12:58:25 -0300

    cambios

--- api-test/index.js
+++ api-test/index.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 const {v4} = require('uuid')
 
-const generarDocumentos = require('./generador_documentos');
-const {handleJson} = require('./tools');
+const generarDocumentos = require('./lib/generador_documentos');
+const {handleJson} = require('./lib/handleJson');
 
 const jsonKey = `JSONS/${v4()}.json`
 
 // Ejemplo de uso:
 const rutaArchivoJson = 'samples/data.json'; // Ruta del archivo JSON que deseas modificar
-const metadata = handleJson(rutaArchivoJson, jsonKey);
-generarDocumentos(metadata, process.env.AWS_S3_SUELDOS_BUCKET, jsonKey)
+
+
+async function ejecutar() {
+    const metadata = await handleJson(rutaArchivoJson, jsonKey);
+    generarDocumentos(metadata, process.env.AWS_S3_SUELDOS_BUCKET, jsonKey);
+  }
+
+ejecutar()
--- api-test/lib/handleJson.js
+++ api-test/lib/handleJson.js
@@ -1,11 +1,9 @@
 require('dotenv').config();
-
-const {v4} = require('uuid')
 const fs = require('fs');
 const AWS = require('aws-sdk');
 
 
-function handleJson(filePath, jsonKey) {
+async function handleJson(filePath, jsonKey) {
 
     const jsonData = fs.readFileSync(filePath);
     const inputJson = JSON.parse(jsonData);
@@ -19,7 +17,7 @@ function handleJson(filePath, jsonKey) {
     const jsonEmployee = inputJson.employee
     fs.writeFileSync("jsonParaSubir.json", JSON.stringify(jsonEmployee, null, 2));
 
-    uploadToS3("jsonParaSubir.json", process.env.AWS_S3_SUELDOS_BUCKET, jsonKey)
+    await uploadToS3("jsonParaSubir.json", process.env.AWS_S3_SUELDOS_BUCKET, jsonKey)
 
     console.log('Campos agregados correctamente al archivo JSON.');
 
