ea83b43be851b49e3a5b33e64b86f150890e0774
MateoF01 <mateofernandez380@gmail.com>
2024-04-04 11:02:28 -0300

    endpoint unico

---
+++ api-test/lib/post.js
@@ -0,0 +1,24 @@
+const { exec } = require('child_process');
+
+function activarStepFunction(url, archivoJSON) {
+    
+    const startExecution = `curl -X POST -H "Content-Type: application/json" -d @${archivoJSON} ${url}`;
+
+    exec(startExecution, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error al enviar la solicitud: ${error.message}`);
+            return;
+        }
+
+        console.log(`Respuesta del servidor: ${stdout}`);
+    });
+}
+
+const urlDestino = 'https://18vlt0z8c8.execute-api.sa-east-1.amazonaws.com/dev/handleJson'
+
+const archivoJSONGrande = '../samples/data.json';
+
+activarStepFunction(urlDestino, archivoJSONGrande);
+
+
+
